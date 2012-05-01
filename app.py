5c7b8e787bd4d43cf6d72144e8ae2466375cae7d
Javier Aravena Claramunt <javier@aravenas.com>
2012-05-01 13:40:48 -0400

    Log model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 class default_config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///asdf.db'
@@ -13,7 +14,15 @@ app.config.from_envvar('INVENTORY_SETTINGS', silent=True)
 
 db = SQLAlchemy(app)
 
+# A word about users:
+# In this particular implementation we have no table for users since they
+# will be always be retrieved from LDAP. That's why all references to user, 
+# in particular in lended_to fields shall be the dn of the user
+
 class AssetType(db.Model):
+    '''
+    Category of the asset, such as "Laptop" or "iPad"
+    '''
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String, unique=True)
     assets = db.relationship('Asset', backref='type')
@@ -25,6 +34,9 @@ class AssetType(db.Model):
         return '<Aset type %r>' % self.description
 
 class Asset(db.Model):
+    '''
+    Particular, singular asset, identified by serial number
+    '''
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
     type_id = db.Column(db.Integer, db.ForeignKey('asset_type.id'))
@@ -37,6 +49,22 @@ class Asset(db.Model):
 
     def __repr__(self):
         return '<Asset %r>' % self.name
+
+class AssetLog(db.Model):
+    '''
+    Log used for accounting. Isn't actually checked by the application
+    '''
+    id = db.Column(db.Integer, primary_key = True)
+    asset_id = db.Column(db.Integer, db.ForeignKey('asset.id'))
+    time = db.Column(db.DateTime(timezone=True))
+    action = db.Column(db.Enum('checkin', 'checkout'))
+    lended_to = db.Column(db.String)
+
+    def __init__(self, asset, action, lended_to):
+        self.time = datetime.now()
+        self.asset_id = asset.id
+        self.action = action
+        self.lended_to = lended_to
 
 @app.route('/')
 def show_assets():
