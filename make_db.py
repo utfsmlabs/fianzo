df6d4f332e7c35fd3b412f5a9cb5ff1debcffdde
Javier Aravena Claramunt <javier@aravenas.com>
2012-05-08 14:58:30 -0400

    setup script makes a few example objects

--- make_db.py
+++ make_db.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python2
 
-from app import db
+from app import db, AssetType, Asset
 db.create_all()
+
+t = AssetType('Laptop')
+db.session.add(t)
+db.session.commit()
+
+for i in range(1, 10):
+    a = Asset('Laptop %d' % i, type = t)
+    db.session.add(a)
+
+db.session.commit()
