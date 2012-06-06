9b7867214f6e0328af8c7ceb249c5961b9160002
Javier Aravena Claramunt <javier@aravenas.com>
2012-06-06 12:36:55 -0400

    Updated asset type form to include loan period
    There's nothing to handle it on the backend though.

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, TextField, PasswordField, validators
+from wtforms import Form, TextField, PasswordField, IntegerField, SelectField, validators
 
 class loginForm(Form):
     username = TextField('Username', [validators.required()])
@@ -8,4 +8,10 @@ class lendForm(Form):
     lended_to = TextField('Username', [validators.required()])
 
 class AssetTypeForm(Form):
+    name = TextField('Name', [validators.required()])
+    loan_period_number = IntegerField('Lend time', [validators.NumberRange(min=0), validators.required()])
+    loan_period_resolution = SelectField('Lend time resolution', [validators.required()], 
+            choices=[('d', 'Days'), ('h', 'Hours'), ('m', 'Minutes')])
+
+class AssetForm(Form):
     name = TextField('Name', [validators.required()])
